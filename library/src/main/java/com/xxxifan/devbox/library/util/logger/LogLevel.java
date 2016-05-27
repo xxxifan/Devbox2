7c9b80cdbe81cbfe73a3c17e6efa39a2b46cd6d6
xxxifan <445947962@qq.com>
2016-05-27 17:26:25 +0800

    add Logger

---
+++ library/src/main/java/com/xxxifan/devbox/library/util/logger/LogLevel.java
@@ -0,0 +1,14 @@
+package com.xxxifan.devbox.library.util.logger;
+
+public enum LogLevel {
+
+    /**
+     * Prints all logs
+     */
+    FULL,
+
+    /**
+     * No log will be printed
+     */
+    NONE
+}
