2dc324e591bff230f2aaa8c38dde76b3a791409e
xxxifan <445947962@qq.com>
2016-06-08 01:52:28 +0800

    trying to make a chained Fragments helper

--- library/src/main/java/com/xxxifan/devbox/library/util/Fragments.java
+++ library/src/main/java/com/xxxifan/devbox/library/util/Fragments.java
@@ -2,43 +2,66 @@ package com.xxxifan.devbox.library.util;
 
 import android.support.annotation.IdRes;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+
+import java.lang.ref.WeakReference;
 
 /**
  * Created by xifan on 6/7/16.
  */
 public class Fragments {
+    // TODO: 6/8/16 new method to prepare a list for no-create checkout
 
-    /**
-     * set fragment will be attach to this activity right now
-     */
-    public static void setContainerFragment(Fragment fragment) {
-        setContainerFragment(fragment, getConfig().getContainerId(), false);
+    public static Operator checkout(FragmentActivity activity, Fragment fragment) {
+        return new Operator(activity, fragment);
     }
 
-    /**
-     * set fragment will be attach to this activity right now
-     *
-     * @param detach if true will detach other fragment instead of hide.
-     */
-    public static void setContainerFragment(Fragment fragment, boolean detach) {
-        setContainerFragment(fragment, getConfig().getContainerId(), detach);
+    public static Operator checkout(FragmentActivity activity, Fragment fragment, String tag) {
+        return new Operator(activity, fragment, tag);
     }
 
-    /**
-     * set fragment will be attach to this activity right now
-     *
-     * @param containerId the target containerId will be attached to.
-     */
-    public static void setContainerFragment(Fragment fragment, @IdRes int containerId) {
-        setContainerFragment(fragment, containerId, false);
-    }
+    public static class Operator {
+        private WeakReference<FragmentActivity> activityRef;
+        private Fragment fragment;
+        private String tag;
+        private FragmentManager fragmentManager;
+        private FragmentTransaction transaction;
 
+        private boolean detachLast;
+        private boolean addToBackStack;
 
-    public static void checkoutFragment(Fragment fragment, @IdRes int containerId, boolean addToBackStack) {
+        public Operator(FragmentActivity activity, Fragment fragment) {
+            this(activity, fragment, fragment.getTag());
+        }
 
-    }
+        public Operator(FragmentActivity activity, Fragment fragment, String tag) {
+            this.activityRef = new WeakReference<>(activity);
+            this.fragment = fragment;
+            this.tag = tag;
+            this.fragmentManager = activity.getSupportFragmentManager();
+            this.transaction = fragmentManager.beginTransaction();
+        }
 
-    public static void checkoutFragment(Fragment fragment, @IdRes int containerId, boolean detach, boolean addToBackStack) {
+        public Operator addSharedElement(View sharedElement, String name) {
+            transaction.addSharedElement(sharedElement, name);
+            return this;
+        }
 
+        public Operator detachLast(boolean detach) {
+            this.detachLast = detachLast;
+            return this;
+        }
+
+        public Operator addToBackStack(boolean add) {
+            this.addToBackStack = add;
+            return this;
+        }
+
+        public void into(@IdRes int containerId) {
+            this.transaction.commit();
+        }
     }
 }
