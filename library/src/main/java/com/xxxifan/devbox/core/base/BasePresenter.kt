697089ed748648ba38cc07a1f2a6d22aea09c959
xxxifan <445947962@qq.com>
2022-01-19 18:50:23 +0800

    upgrade lifecycle

--- library/src/main/java/com/xxxifan/devbox/core/base/BasePresenter.kt
+++ library/src/main/java/com/xxxifan/devbox/core/base/BasePresenter.kt
@@ -1,15 +1,13 @@
 package com.xxxifan.devbox.core.base
 
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.OnLifecycleEvent
 import androidx.lifecycle.coroutineScope
 
 /**
  * Created by xifan on 17-8-1.
  */
-interface BasePresenter<T> : LifecycleObserver {
+interface BasePresenter<T> : DefaultLifecycleObserver {
   var view: T?
 
   val lifecycleOwner: LifecycleOwner?
@@ -21,8 +19,8 @@ interface BasePresenter<T> : LifecycleObserver {
   val lifecycleScope
     get() = lifecycleOwner?.lifecycle?.coroutineScope
 
-  @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
-  fun onDestroy(owner: LifecycleOwner) {
+
+  override fun onDestroy(owner: LifecycleOwner) {
     view = null
     owner.lifecycle.removeObserver(this)
   }
