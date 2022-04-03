9d28f733afb4d19cce0c7ccacb7b130f42ac7211
xxxifan <445947962@qq.com>
2022-04-04 00:33:30 +0800

    demo

---
+++ app/src/main/java/com/xxxifan/devbox/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.xxxifan.devbox
+
+import android.os.Bundle
+import androidx.viewbinding.ViewBinding
+import com.xxxifan.devbox.core.base.BaseActivity
+
+//
+// Created by xxxifan on 2022/2/23.
+//
+class MainActivity :BaseActivity() {
+  override val bind: ViewBinding
+    get() = TODO("Not yet implemented")
+
+  override fun onSetupActivity(savedInstanceState: Bundle?) {
+    TODO("Not yet implemented")
+  }
+}
